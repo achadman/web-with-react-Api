48abcb50730e370b80250c8ba2b0bee6d76b9587
achadman <aditya78maulana@gmail.com>
2025-09-08 09:08:20 +0700

    upload all files

---
+++ src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "8d19755c3aec97101f095c1eb3e2f7b2";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
